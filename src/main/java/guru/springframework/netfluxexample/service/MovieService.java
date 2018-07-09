4106ced54931b62e1d0a785c9061174e23201f5e
Nirav Joshi <joshi.nirav21@gmail.com>
2018-07-09 11:05:36 +0530

    Initial commit

---
+++ src/main/java/guru/springframework/netfluxexample/service/MovieService.java
@@ -0,0 +1,12 @@
+package guru.springframework.netfluxexample.service;
+
+import guru.springframework.netfluxexample.domain.Movie;
+import guru.springframework.netfluxexample.domain.MovieEvent;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface MovieService {
+    Flux<MovieEvent> events(String eventId);
+    Mono<Movie> getMovieById(String id);
+    Flux<Movie> getAllMovies();
+}
